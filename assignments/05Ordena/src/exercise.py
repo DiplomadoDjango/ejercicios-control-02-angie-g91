c4e67f8ce64025483503220a4554047262c6d656
Angie García <162246583+angie-g91@users.noreply.github.com>
2024-03-12 23:00:11 -0600

    Update exercise.py

--- assignments/05Ordena/src/exercise.py
+++ assignments/05Ordena/src/exercise.py
@@ -1,9 +1,35 @@
 def main():
-    # Escribe el código adecuado para completar el programa
-    x = int(input("Ingresa el primer número: "))
-    y = int(input("Ingresa el segundo número: "))
-    z = int(input("Ingresa el tercer número: "))
+    try:
+        x = int(input("Ingresa el primer número: "))
+        y = int(input("Ingresa el segundo número: "))
+        z = int(input("Ingresa el tercer número: "))
+        
+        if x <= y and x <= z:
+            print(x)
+            if y <= z:
+                print(y)
+                print(z)
+            else:
+                print(z)
+                print(y)
+        elif y <= x and y <= z:
+            print(y)
+            if x <= z:
+                print(x)
+                print(z)
+            else:
+                print(z)
+                print(x)
+        else:
+            print(z)
+            if x <= y:
+                print(x)
+                print(y)
+            else:
+                print(y)
+                print(x)
+    except ValueError:
+        print("Por favor, ingresa solo números enteros.")
 
-
-if __name__=='__main__':
+if __name__ == "__main__":
     main()
