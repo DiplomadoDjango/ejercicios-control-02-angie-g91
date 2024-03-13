8389876f22618aef4ae79dafe677ed2cc88880a0
Angie García <162246583+angie-g91@users.noreply.github.com>
2024-03-12 23:02:57 -0600

    Update exercise.py

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -1,25 +1,20 @@
-
-import math
-
 def main():
-    try:
-        edad = int(input("Ingresa tu edad: "))
-        if edad < 0:
-            print("No cumples requisitos")
-            return
-        
-        identificacion = input("¿Tienes identificación oficial? (s/n): ")
-        if identificacion.lower() == 's':
-            if edad >= 18:
-                print("Trámite de licencia concedido")
-            else:
-                print("No cumples requisitos")
-        elif identificacion.lower() == 'n':
-            print("No cumples requisitos")
-        else:
-            print("Respuesta incorrecta")
-    except ValueError:
+    edad = int(input("Ingresa tu edad: "))
+    if edad < 0:
         print("No cumples requisitos")
+        return
+    
+    identificacion = input("¿Tienes identificación oficial? (s/n): ")
+    if identificacion.lower() == 's':
+        if edad >= 18:
+            print("Trámite de licencia concedido")
+        else:
+            print("No cumples requisitos")
+    elif identificacion.lower() == 'n':
+        print("No cumples requisitos")
+    else:
+        print("Respuesta incorrecta")
 
 if __name__ == "__main__":
     main()
+
