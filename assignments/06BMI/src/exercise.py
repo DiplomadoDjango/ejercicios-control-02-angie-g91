77b2f3c5ef09d27b59b440c795f118592134cde9
Angie García <162246583+angie-g91@users.noreply.github.com>
2024-03-12 23:49:30 -0600

    Update exercise.py

--- assignments/06BMI/src/exercise.py
+++ assignments/06BMI/src/exercise.py
@@ -1,8 +1,24 @@
+def calcular_imc(peso, altura):
+    indice = peso / (altura ** 2)
+    if peso <= 0 or altura <= 0:
+        return "Revisa tus datos, alguno de ellos es erróneo."
+    elif indice < 20:
+        return "PESO BAJO"
+    elif 20 <= indice < 25:
+        return "NORMAL"
+    elif 25 <= indice < 30:
+        return "SOBREPESO"
+    elif 30 <= indice < 40:
+        return "OBESIDAD"
+    else:
+        return "OBESIDAD MORBIDA"
+
 def main():
-    #escribe tu código abajo de esta línea
     peso = float(input("Peso en kg: "))
     altura = float(input("Altura en m: "))
     
+    resultado = calcular_imc(peso, altura)
+    print(resultado)
 
-if __name__=='__main__':
-    main()
+
+if __name__ == "__main__":
