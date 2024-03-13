7f2f3698a0e8f1c65d5224e877b58daaeaa15c1a
Angie García <162246583+angie-g91@users.noreply.github.com>
2024-03-12 23:03:47 -0600

    Update exercise.py

--- assignments/01IdentificaSigno/src/exercise.py
+++ assignments/01IdentificaSigno/src/exercise.py
@@ -1,17 +1,12 @@
-import math
-
 def main():
-    try:
-        numero = int(input("Dame un número: "))
-        
-        if numero > 0:
-            print("Es positivo")
-        elif numero < 0:
-            print("Es negativo")
-        else:
-            print("Es cero")
-    except ValueError:
-        print("Por favor ingresa un número entero válido.")
+    numero = int(input("Dame un número: "))
+    
+    if numero > 0:
+        print("Es positivo")
+    elif numero < 0:
+        print("Es negativo")
+    else:
+        print("Es cero")
 
 if __name__ == "__main__":
     main()
